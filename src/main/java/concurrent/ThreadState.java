d7becd8ac3f40572173276015636af7cff9d2834
Vlad <vladbaykin@gmail.com>
2021-08-02 22:55:21 +0300

    Состояние нити.

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,19 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " - " + first.getState());
+            System.out.println(second.getName() + " - " + second.getState());
+        }
+        System.out.println("работа завершена");
+    }
+}
